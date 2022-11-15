96e20af2b730deb0259b26fb6631107d92e3ffe0
woshahua <wohangye@gmail.com>
2022-11-16 01:35:17 +0900

    Initial Commit

---
+++ OpenZen/OpenZenApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenZenApp.swift
+//  OpenZen
+//
+//  Created by gaohang on 2022/11/16.
+//
+
+import SwiftUI
+
+@main
+struct OpenZenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
