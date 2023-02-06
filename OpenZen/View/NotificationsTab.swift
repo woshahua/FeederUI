7c4aa827d062a12bfae8ae2fdad68afa67eac692
woshahua <wohangye@gmail.com>
2023-02-06 23:49:11 +0900

    initial commit

---
+++ OpenZen/View/NotificationsTab.swift
@@ -0,0 +1,16 @@
+//
+//  NotificationsTab.swift
+//  OpenZen
+//
+//  Created by gaohang on 2023/02/04.
+//
+
+import Foundation
+import SwiftUI
+
+struct NotificationsTab: View {
+    @StateObject private var routerPath =
+    var body: some View {
+        NavigationStack(p)
+    }
+}
--- OpenZen/View/SectionView.swift
+++ OpenZen/View/SectionView.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct SectionView: View {
+    // TODO: how to creaate mock data randomly
+    @State let data: [FeedData] = [FeedData(id: "1", title: "2", description: "345")]
     var body: some View {
-        VStack {
-            
+        HStack {
+            Image(systemName: "")
         }
     }
+}
+
+struct FeedData: Codable {
+    let id: String
+    let title: String
+    let description: String
 }
